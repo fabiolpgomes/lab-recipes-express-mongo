a41026db98b60ad7348dd5fc036f5ebf79019a9f
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-09-23 16:28:50 +0200

    Included {new: true};

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 // Importe os models aqui
 const RecipeModel = require("../models/Recipe.model");
+const { findByIdAndUpdate } = require("../models/User.model");
 const UserModel = require("../models/User.model");
 
 //1º rota: Criar uma receita   Iteration 2 - Create a recipe
@@ -28,6 +29,21 @@ router.get("/all", async (req, res) => {
     return res.status(400).json(error);
   }
 });
+
+
+
+//Post Create route POST /:Id
+ router.post ("/:Id", async (req, res) => {
+ const { Id } = req.params; 
+ try {
+   const recipe = await RecipeModel.findByIdAndUpdate(Id,{...req.body}, {new: true});
+   return res.status(200).json(recipe);
+ } catch (error) {
+   console.log(error);
+   return res.status(400).json(error);
+ }
+});
+
 
 //3º rota: Acessar uma única receita pelo seu ID
 router.get("/:id", async (req, res) => {
@@ -77,7 +93,7 @@ router.get("/dislikesusers/:id", async (req, res) => {
   }
 });
 
-//!5º rota: Deletar uma receita pelo seu ID - retira-la da array de favorites e dislikes dos USERS
+// - retira-la da array de favorites e dislikes dos USERS
 router.delete("/recepie/delete/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -94,4 +110,3 @@ router.delete("/recepie/delete/:id", async (req, res) => {
 //Não se esqueça de exportar o router!
 module.exports = router;
 
-// Incluir 2 campos Likes e Dislikes no models Recipe.model.js
